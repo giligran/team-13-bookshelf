4dae81109f68ec6e2045270263cd154745e472c6
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-06-30 05:33:06 +0300

    some js

---
+++ src/js/shopping-list.js
@@ -0,0 +1,33 @@
+const bookList = document.getElementById('book-list');
+
+const savedBooks = JSON.parse(localStorage.getItem('shoppingListBooks'));
+
+if (savedBooks && savedBooks.length > 0) {
+  // Проходимося по кожній збереженій книзі
+  savedBooks.forEach(book => {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book-card');
+    bookElement.innerHTML = `
+      <img src="${book.image}" alt="Зображення обгортки книги">
+      <h2 class="book-title">${book.title}</h2>
+      <h3 class="book-category">${book.category}</h3>
+      <p class="book-description">${book.description}</p>
+      <p class="book-author">${book.author}</p>
+      <ul class="book-retailers">
+        ${book.retailers
+          .map(
+            retailer =>
+              `<li><a href="${retailer.link}">${retailer.name}</a></li>`
+          )
+          .join('')}
+      </ul>
+      <button class="remove-button">Видалити зі списку покупок</button>
+    `;
+    bookList.appendChild(bookElement);
+  });
+} else {
+  const noBooksImage = document.createElement('img');
+  noBooksImage.src = '../img/blocks.png';
+  noBooksImage.alt = 'Зображення порожнього списку покупок';
+  bookList.appendChild(noBooksImage);
+}
