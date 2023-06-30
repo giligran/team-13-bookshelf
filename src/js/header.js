409284ccbdedddc3cb48512741be59ed83c26cf8
Vorongor <mr.maddarknes@gmail.com>
2023-06-30 14:31:33 +0300

    ex2

---
+++ src/js/header.js
@@ -0,0 +1,10 @@
+const switcherRef = document.querySelector('#switch');
+
+function themasChanger (evt) {
+    console.log(evt.target);
+    // if (condition) {
+        
+    // }
+}
+
+switcherRef.addEventListener('click', themasChanger())
