35336e27ee2cb2c09736f19c28e51e8f8dca6730
giligran <kuprykovrodion@gmail.com>
2023-07-01 16:51:02 +0300

    fix index

--- src/js/modal-authorization.js
+++ src/js/modal-authorization.js
@@ -12,25 +12,28 @@ function onSubmit(e) {
   registerNewUser(name.value, email.value, password.value);
   form.reset();
 }
+console.log(form.innerHTML);
+
+form.innerHTML = '';
 
 // Sign Up / Sign in
-const inBtn = document.getElementById('InBtn');
-const upBtn = document.getElementById('upBtn');
-const nameBox = document.getElementById('name');
-const subBtn = document.getElementById('submit');
+// const inBtn = document.getElementById('InBtn');
+// const upBtn = document.getElementById('upBtn');
+// const nameBox = document.getElementById('name');
+// const subBtn = document.getElementById('submit');
 
-function regOn(evt) {
-  nameBox.classList.remove('hide');
-  inBtn.classList.add('active');
-  upBtn.classList.remove('active');
-  subBtn.textContent = 'Sign In';
-}
-function regOff(evt) {
-  nameBox.classList.add('hide');
-  inBtn.classList.remove('active');
-  upBtn.classList.add('active');
-  subBtn.textContent = 'Sign Up';
-}
+// function regOn(evt) {
+//   nameBox.classList.remove('hide');
+//   inBtn.classList.add('active');
+//   upBtn.classList.remove('active');
+//   subBtn.textContent = 'Sign In';
+// }
+// function regOff(evt) {
+//   nameBox.classList.add('hide');
+//   inBtn.classList.remove('active');
+//   upBtn.classList.add('active');
+//   subBtn.textContent = 'Sign Up';
+// }
 
-inBtn.addEventListener('click', regOn);
-upBtn.addEventListener('click', regOff);
+// inBtn.addEventListener('click', regOn);
+// upBtn.addEventListener('click', regOff);
