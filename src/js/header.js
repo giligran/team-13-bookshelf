5f83e5268a2143a46fcecbc98a733b47bce66635
Vorongor <mr.maddarknes@gmail.com>
2023-07-01 11:22:01 +0300

    Ex5

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,29 @@
-const refs ={
-    switcherRef: document.querySelector('.switch'),
-    container: document.querySelector('.header-container'),
-    logo: document.querySelector('.icon-title-svg'),
-    shop: document.querySelector('.shop-icon-svg'),
-    arrow: document.querySelector('.arow-down'),
-    mobMenu: document.querySelector('.icon-menu-svg'),
-}
-
-// const switcherRef = document.querySelector('.switch');
-
-function themasChanger (evt) {
-    if (evt.target.nodeName === "INPUT") {
+//  Thema Changer
+const refs = {
+  switcherRef: document.querySelector('.switch'),
+  container: document.querySelector('.header-container'),
+  logo: document.querySelector('.icon-title-svg'),
+  shop: document.querySelector('.shop-icon-svg'),
+  arrow: document.querySelector('.arow-down'),
+  burger: document.querySelector('.icon-menu-svg'),
+  mobMenu: document.querySelector('#mobile-menu'),
+  mobOpen: document.querySelector('#mob-menu-open'),
+  mobClose: document.querySelector('#mob-menu-close'),
+};
+function themasChanger(evt) {
+  if (evt.target.nodeName === 'INPUT') {
     refs.container.classList.toggle('dark');
     refs.logo.classList.toggle('dark');
     refs.shop.classList.toggle('dark');
-    refs.arrow.classList.toggle('dark');
-    refs.mobMenu.classList.toggle('dark');
-    
-    }
+    refs.burger.classList.toggle('dark');
+  }
 }
+refs.switcherRef.addEventListener('click', themasChanger);
 
-refs.switcherRef.addEventListener('click', themasChanger)
+// Open mobile menu
+refs.mobOpen.addEventListener('click', () => {
+  refs.mobMenu.classList.toggle('hidden');
+});
+refs.mobClose.addEventListener('click', () => {
+  refs.mobMenu.classList.toggle('hidden');
+});
