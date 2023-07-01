d9d694222ac24a739aece1bb2fd405c18080cd53
giligran <kuprykovrodion@gmail.com>
2023-07-01 22:03:25 +0300

    change index

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -11,66 +11,24 @@ import {
 } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-const LOCAL_NAME = 'token-firebase';
+const localKey = 'user';
 
-// Your web app's Firebase configuration
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 const database = getDatabase(app);
 
-// const user = auth.currentUser;
-// console.log(currentUser);
-
-onAuthStateChanged(auth, user => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    const uid = user.uid;
-    // console.log(user); // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
-
+const user = auth.currentUser;
 export const signUpUser = async (name, email, password) => {
   const newUser = await createUserWithEmailAndPassword(auth, email, password);
   const userCredential = await newUser;
+  signInUser(email, password);
+  localStorage.setItem(localKey + '-' + name, userCredential.user);
   console.log(userCredential);
 };
-
-// export function signUpUser(name, email, password) {
-//   return createUserWithEmailAndPassword(auth, email, password)
-//     .then(userCredential => {
-//       const user = userCredential.user;
-//     })
-//     .then(data => console.log(data))
-//     .catch(error => {
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//       console.error('Помилка реєстрації:', errorCode, errorMessage);
-//     });
-// }
 
 export const signInUser = async (email, password) => {
   const currentUser = await signInWithEmailAndPassword(auth, email, password);
   const userCredential = await currentUser;
+
   console.log(userCredential);
 };
-
-// function checkAuth(user) {
-//   onAuthStateChanged(auth, user => {
-//     if (user) {
-//       // User is signed in, see docs for a list of available properties
-//       // https://firebase.google.com/docs/reference/js/auth.user
-//       const uid = user.uid;
-//       // ...
-//     } else {
-//       // User is signed out
-//       // ...
-//     }
-//   });
-// }
-
-// console.log(localStorage.getItem(fbase_key));
--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import 'modern-normalize/modern-normalize.css';
+
+import { auth } from './js/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+onAuthStateChanged(auth, user => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    console.dir(user);
+    console.log(uid);
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
