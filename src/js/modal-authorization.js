8740ff2a54d3118f851cd43b07b2a5438f4af098
giligran <kuprykovrodion@gmail.com>
2023-07-01 17:06:58 +0300

    fix form auth

--- src/js/modal-authorization.js
+++ src/js/modal-authorization.js
@@ -3,6 +3,19 @@ import '../css/modal-authorization.css';
 import { registerNewUser } from './firebase';
 
 const form = document.querySelector('.sign-up-form');
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+console.log(radioButtons);
+
+radioButtons.forEach(radioButton => {
+  radioButton.addEventListener('change', () => {
+    if (radioButton.value === 'sign-in') {
+      renderFormSignIn();
+    } else {
+      renderFormSignUp();
+    }
+  });
+});
 
 form.addEventListener('submit', onSubmit);
 
@@ -12,9 +25,65 @@ function onSubmit(e) {
   registerNewUser(name.value, email.value, password.value);
   form.reset();
 }
-console.log(form.innerHTML);
 
-form.innerHTML = '';
+function renderFormSignUp() {
+  console.log('passed singIn function');
+  form.innerHTML = `<label for="name" class="visually-hidden">name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="name"
+        required
+        class="sign-up-form-input name hide"
+      />
+
+      <label for="email" class="visually-hidden">eMAIL</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="eMAIL"
+        required
+        class="sign-up-form-input"
+      />
+
+      <label for="password" class="visually-hidden">pASSWORD</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="pASSWORD"
+        required
+        class="sign-up-form-input"
+      />
+
+      <button type="submit" class="submit-btn" id="submit">Sign up</button>`;
+}
+function renderFormSignIn() {
+  console.log('passed singUp function');
+  form.innerHTML = `<label for="email" class="visually-hidden">eMAIL</label>
+  <input
+    type="email"
+    id="email"
+    name="email"
+    placeholder="eMAIL"
+    required
+    class="sign-up-form-input"
+  />
+
+  <label for="password" class="visually-hidden">pASSWORD</label>
+  <input
+    type="password"
+    id="password"
+    name="password"
+    placeholder="pASSWORD"
+    required
+    class="sign-up-form-input"
+  />
+
+  <button type="submit" class="submit-btn" id="submit">Sign in</button>`;
+}
 
 // Sign Up / Sign in
 // const inBtn = document.getElementById('InBtn');
