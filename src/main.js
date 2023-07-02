d14b46dbcb9b0a7c0b27b08f8b6e74931c799deb
giligran <kuprykovrodion@gmail.com>
2023-07-02 16:00:19 +0300

    backup stable version

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import 'modern-normalize/modern-normalize.css';
 
-// import { auth } from './js/firebase';
-// import { checkAuth } from './js/firebase';
+import { auth } from './js/firebase';
+import { checkAuth } from './js/firebase';
 
-// // проверка авторизации при загрузке
+// проверка авторизации при загрузке
 
-// window.addEventListener('DOMContentLoaded', function () {
-//   checkAuth();
-// });
+window.addEventListener('DOMContentLoaded', function () {
+  checkAuth();
+});
