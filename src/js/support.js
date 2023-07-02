f2da989573c09aeead99b8893e1029f4f487aeb2
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-07-03 00:24:10 +0300

    added png-files

--- src/js/support.js
+++ src/js/support.js
@@ -3,47 +3,47 @@ const funds = [
     title: 'Save the Children',
     url:
       'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: './img/logo-partners/save-the-children.png',
+    img: './img/logo-partners/save-the-children@2x.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: '../img/logo-partners/project-hope.png',
+    img: '../img/logo-partners/hope@2x.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: "/img/logo-partners/united24.png",
+    img: "/img/logo-partners/u24@2x.png",
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: "./img/logo-partners/imc.png",
+    img: "./img/logo-partners/imc@2x.png",
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: "./img/logo-partners/msf.png",
+    img: "./img/logo-partners/msf@2x.png",
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: "./img/logo-partners/razom.png",
+    img: "./img/logo-partners/razom@2x.png",
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: "./img/logo-partners/aah.png",
+    img: "./img/logo-partners/aah@2x.png",
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: "./img/logo-partners/world-vision.png",
+    img: "./img/logo-partners/world-vision@2x.png",
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: './img/logo-partners/ch-sp.png',
+    img: './img/logo-partners/prytula@2x.png',
   },
 ]
 
