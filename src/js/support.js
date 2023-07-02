46a10d0ebbf3e861a334871f0b501ac4300e120a
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-07-02 23:35:20 +0300

    corrected some mistakes

--- src/js/support.js
+++ src/js/support.js
@@ -3,17 +3,17 @@ const funds = [
     title: 'Save the Children',
     url:
       'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: "./img/logo-partners/save-the-children.png",
+    img: './img/logo-partners/save-the-children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: "./img/logo-partners/project-hope.png",
+    img: '../img/logo-partners/project-hope.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: "./img/logo-partners/united24.png",
+    img: "/img/logo-partners/united24.png",
   },
   {
     title: 'International Medical Corps',
@@ -47,27 +47,82 @@ const funds = [
   },
 ]
 
-
 const fundsList = document.getElementById('funds-list');
+const supportPrevBtn = document.querySelector('.support-prev');
+const supportNextBtn = document.querySelector('.support-next');
+const visibleItemCount = 4; 
+let currentSlideIndex = 0;
 
-funds.forEach((fund) => {
+function showNextItems() {
+  const nextSlideIndex = currentSlideIndex + visibleItemCount;
+  fundsList.innerHTML = ''; 
+  for (let i = currentSlideIndex; i < nextSlideIndex && i < funds.length; i++) {
+    const fund = funds[i];
     const item = document.createElement('li');
     const link = document.createElement('a');
     const img = document.createElement('img');
-    
+
+    link.target = '_blank';
+    link.rel = 'noreferrer noopener';
+
     item.classList.add('support-item');
-    link.classList.add('support-link')
+    link.classList.add('support-link');
     img.classList.add('support-img');
-    
+
     link.href = fund.url;
     img.src = fund.img;
-    
+
     link.appendChild(img);
     item.appendChild(link);
     fundsList.appendChild(item);
+
+    link.dataset.slideIndex = i;
+  }
+  currentSlideIndex = nextSlideIndex;
+  updateNavigationButtons();
+}
+
+function showPreviousItems() {
+  const prevSlideIndex = Math.max(currentSlideIndex - visibleItemCount, 0);
+  fundsList.innerHTML = ''; 
+  for (let i = prevSlideIndex; i < currentSlideIndex; i++) {
+    const fund = funds[i];
+    const item = document.createElement('li');
+    const link = document.createElement('a');
+    const img = document.createElement('img');
+
+    link.target = '_blank';
+    link.rel = 'noreferrer noopener';
+
+    item.classList.add('support-item');
+    link.classList.add('support-link');
+    img.classList.add('support-img');
+
+    link.href = fund.url;
+    img.src = fund.img;
+
+    link.appendChild(img);
+    item.appendChild(link);
+    fundsList.appendChild(item);
+
+    link.dataset.slideIndex = i;
+  }
+  currentSlideIndex = prevSlideIndex;
+  updateNavigationButtons();
+}
+
+function updateNavigationButtons() {
+  supportPrevBtn.disabled = currentSlideIndex === 0;
+  supportNextBtn.disabled = currentSlideIndex + visibleItemCount >= funds.length;
+}
+
+supportNextBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  showNextItems();
 });
 
+supportPrevBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+});
 
-
-
-
+showNextItems();
