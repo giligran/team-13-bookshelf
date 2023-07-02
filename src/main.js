053437837c3a188e927fa93df200c14e569bd3c2
giligran <kuprykovrodion@gmail.com>
2023-07-02 16:42:17 +0300

    backup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import 'modern-normalize/modern-normalize.css';
 
 import { auth } from './js/firebase';
-import { checkAuth } from './js/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
-// проверка авторизации при загрузке
-
-window.addEventListener('DOMContentLoaded', function () {
-  checkAuth();
+onAuthStateChanged(auth, user => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    console.dir(user);
+    console.log(uid);
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
 });
