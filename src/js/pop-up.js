f5a015feda18b6bb557830397bc40c0babaa42b5
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2023-07-04 22:48:57 +0300

    update pop-up css js html

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,82 +1,46 @@
 (() => {
-    const refs = {
-      openCardBtn: document.querySelector("[data-card-open]"),
-      closeCardBtn: document.querySelector("[data-card-close]"),
-      card: document.querySelector("[data-card]"),
-    };
-  
-    refs.openCardBtn.ad("click", toggleCard);
-    refs.closeCardBtn.addEventListener("click", toggleCard);dEventListener
-  
-    function toggleCard() {
-      refs.card.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
+  const refs = {
+    openCardBtn: document.querySelector("[data-card-open]"),
+    closeCardBtn: document.querySelector("[data-card-close]"),
+    card: document.querySelector("[data-card]"),
+  };
 
-  export function fetchBook(bookId) {
-    return fetch(
-      `https://books-backend.p.goit.global/books/search?book_ids=${bookId}`,
-      {
-        headers: {
-          'x-api-key':
-            'live_8iFJ0bMrHFUFWpFRitEuYrSXz8ohqNsUgTO4QDa1A0a93kKesyd5PokSqrb2ntrV',
-        },
+  refs.openCardBtn.addEventListener("click", toggleCard);
+  refs.closeCardBtn.addEventListener("click", toggleCard);
+      function toggleCard() {
+    refs.card.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
+
+function fetchDataAndPopulateModal() {
+  const apiUrl = 'https://books-backend.p.goit.global/books/643282b1e85766588626a0ba';
+  
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch book data');
       }
-    )
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch book by books');
-        }
-        return response.json();
-      })
-      .then(data => {
-      const book = data[0];
-      renderBookCard(book);
-      return book;
-  })
-  .catch(error => {
+      return response.json();
+    })
+    .then((data) => {
+      const backdrop = document.querySelector('[data-card]');
+      const bookTitle = document.querySelector('.book-title');
+      const authorName = document.querySelector('.author');
+      const bookDescription = document.querySelector('.info-book');
+      const bookShopImages = document.querySelectorAll('.book-shop img');
+      
+      bookTitle.textContent = data.title;
+      authorName.textContent = data.author;
+      bookDescription.textContent = data.description;
+      
+      bookShopImages.forEach((image, index) => {
+        image.src = data.shopLogos[index];
+      });
+      
+      backdrop.classList.remove('is-hidden');
+    })
+    .catch((error) => {
       console.error(error);
-      throw error;
-  });
-  }
-  
-  function renderBookCard(book) {
-    const backdrop = document.querySelector('[data-card]');
-    backdrop.classList.remove('is-hidden');
-  
-    const bookTitle = document.querySelector('.book-title');
-    bookTitle.textContent = book.title;
-  
-    const authorName = document.querySelector('.author');
-    authorName.textContent = book.author;
-  
-    const bookDescription = document.querySelector('.info-book');
-    bookDescription.textContent = book.description;
-    
-    const bookShopImages = document.querySelectorAll('.book-shop img');
-    bookShopImages.forEach((image, index) => {
-      image.src = book.shopLogos[index];
     });
-  
-  }
-
-  
-const addButton = document.querySelector('.card-button');
-
-addButton.addEventListener('click', toggleBook);
-
-function toggleBook() {
-  const bookId = 'bookid'; 
-  
-  if (localStorage.getItem(bookId)) {    
-    localStorage.removeItem(bookId);
-    addButton.textContent = 'Remove from shopping list'; 
-  } else {    
-    localStorage.setItem(bookId, 'true');
-    addButton.textContent = 'Add to shopping list'; 
-
-
-  }
 }
-  
