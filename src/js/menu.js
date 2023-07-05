ae95d4a7f8c597b4682dcfeab02d0ed292fe3689
Vorongor <mr.maddarknes@gmail.com>
2023-07-05 15:52:18 +0300

    Working mob menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,33 +4,41 @@ const mobMenu = document.querySelector('#mobile-menu');
 const container = document.querySelector('.header-container');
 const mobOpen = document.querySelector('#mob-menu-open');
 const mobClose = document.querySelector('#m-close');
+const mobBtmSignUp = document.querySelector('#btn-signup');
+const avatar = document.querySelector('#avatar');
+const blokLink = document.querySelector('#blokLinkMb');
+const mobileUN = document.querySelector('#mobileUN');
 
-if (localStorage.getItem('exist')) {
-  const userName = document.querySelector('#mobileUN');
-  const logoutMenuBtn = document.querySelector('#logout-button');
-  userName.textContent = `${localStorage.getItem('name')}`;
-  logoutMenuBtn.style.display = 'block';
-  logoutButton.addEventListener('click', logOut);
-} else {
-  const avatar = document.querySelector('#avatar');
-  avatar.textContent = '';
-  logoutButton.textContent = 'Log In';
-  logoutButton.removeEventListener('click', logOut);
-  logoutButton.addEventListener('click', logIN);
+export function mobileAuthChek() {
+  if (localStorage.getItem('exist')) {
+    mobBtmSignUp.style.display = 'none';
+    avatar.style.display = 'flex';
+    blokLink.style.display = 'flex';
+    logoutButton.style.display = 'flex';
+    mobileUN.textContent = `${localStorage.getItem('name')}`;
+  } else {
+    mobBtmSignUp.style.display = 'block';
+    avatar.style.display = 'none';
+    blokLink.style.display = 'none';
+    logoutButton.style.display = 'none';
+  }
 }
-console.log(localStorage.getItem('exist'));
 
 function logOut(e) {
   e.preventDefault();
   localStorage.clear();
   location.reload();
-  console.log('reloaded');
 }
 
 function logIN() {
-  backdrop.classList.toggle('visually-hidden');
-  mobMenu.classList.toggle('hidden');
-  container.classList.toggle('fixed');
-  mobOpen.classList.toggle('inactive');
-  mobClose.classList.toggle('active');
+  backdrop.classList.remove('visually-hidden');
+  mobMenu.classList.add('hidden');
+  container.classList.remove('fixed');
+  mobOpen.classList.remove('inactive');
+  mobClose.classList.remove('active');
+  console.log('2');
 }
+mobBtmSignUp.addEventListener('click', logIN);
+logoutButton.addEventListener('click', logOut);
+
+
